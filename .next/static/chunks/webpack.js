9ba7b539d83d1838d95428c3a300cf2d0d1466e4
dhanu1402 <dhanukumar2000@gmail.com>
2023-09-23 00:18:22 +0530

    fetch post by category

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef2d85889bf98478"; }
+/******/ 		__webpack_require__.h = function() { return "4a39ccd2d0192a29"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
