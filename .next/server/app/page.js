6fbfebdc9b24646ee46c0c0ae6b0df38f353867e
dhanu1402 <dhanukumar2000@gmail.com>
2023-09-23 12:03:10 +0530

    prisma1

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -333,11 +333,11 @@ const routeModule = new AppPageRouteModule({
 
 /***/ }),
 
-/***/ 58166:
+/***/ 57210:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 73380, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 50954, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 73380, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 88207))
 
 /***/ }),
